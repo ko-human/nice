d329fbdd3285a91d2187bac37a72f8ada1d311bb
jyj <whdpwl0421@naver.com>
2020-08-28 09:07:42 +0900

    mobile search slide

--- script/ui_common_m.js
+++ script/ui_common_m.js
@@ -20,3 +20,49 @@ var uiCommon = {
     dateFormat: "yy-mm-dd"
   }
 };
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */
+        } else {
+            /* right swipe */
+        }
+    } else {
+        if ( yDiff > 0 ) {
+           $(".quick_wrap").addClass("on");
+        } else {
+            $(".quick_wrap").removeClass("on");
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
